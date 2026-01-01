4b728cd38955a4b812ddb2c14d70de0d8871c9d0
tafifa <tafifa09@gmail.com>
2026-01-02 02:52:02 +0700

    refactor: update routing (Cara Kerja → Fitur) and global styles

--- src/App.tsx
+++ src/App.tsx
@@ -3,8 +3,16 @@ import { Toaster as Sonner } from "@/components/ui/sonner";
 import { TooltipProvider } from "@/components/ui/tooltip";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
+import ScrollToTop from "./components/ScrollToTop";
+import ScrollToTopButton from "./components/ScrollToTopButton";
 import Index from "./pages/Index";
 import NotFound from "./pages/NotFound";
+import PrivacyPolicy from "./pages/PrivacyPolicy";
+import TermsOfService from "./pages/TermsOfService";
+import TentangKami from "./pages/TentangKami";
+import Fitur from "./pages/Fitur";
+import ListKuliner from "./pages/ListKuliner";
+import FAQ from "./pages/FAQ";
 
 const queryClient = new QueryClient();
 
@@ -14,8 +22,14 @@ const App = () => (
       <Toaster />
       <Sonner />
       <BrowserRouter>
+        <ScrollToTop />
+        <ScrollToTopButton />
         <Routes>
           <Route path="/" element={<Index />} />
+          <Route path="/tentang-kami" element={<TentangKami />} />
+          <Route path="/fitur" element={<Fitur />} />
+          <Route path="/list-kuliner" element={<ListKuliner />} />
+          <Route path="/faq" element={<FAQ />} />
           {/* ADD ALL CUSTOM ROUTES ABOVE THE CATCH-ALL "*" ROUTE */}
           <Route path="*" element={<NotFound />} />
         </Routes>
